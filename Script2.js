3e0f9515eaf20a9e18c84e40ea52def8e95c1e22
AltMirrorBell <410762240@qq.com>
2020-07-15 10:38:55 +0800

    Create Script2.js
    自动跳过广告

---
+++ Script2.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         NGA自动跳过广告
+// @version      1.0
+// @author       AltMirrorBell
+// @description  rt
+// @icon         https://s1.ax1x.com/2020/06/28/N25WBF.png
+// @match        *://bbs.nga.cn/misc/adpage*
+// @inject-into content
+// ==/UserScript==
+
+(function() {
+    getJump();
+})();
